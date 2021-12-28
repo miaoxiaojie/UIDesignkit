741ce023003a0142042f81a0ccc8ff06d3319696
miaomiao.zhao <miaomiao.zhao@zhaopin.com.cn>
2021-12-28 11:12:21 +0800

    尝试

--- UIDesignkit/UIDesignkit/Classes/Avatar/UIImageViewExtensions.swift
+++ UIDesignkit/UIDesignkit/Classes/Avatar/UIImageViewExtensions.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 public extension UIImageView {
-    func asAvatar(cornerRadius: CGFloat = 6) {
+    func asAvatar(cornerRadius: CGFloat = 4) {
         layer.cornerRadius = cornerRadius
         clipsToBounds = true
     }
